6f18ecc123902cce5fd1d6a49119b07c4eb334f2
Pe Pe <pszemek.prabucki@gmail.com>
2021-11-14 20:35:08 +0100

    add levels
    even numbered

--- Assets/Scripts/Wand.cs
+++ Assets/Scripts/Wand.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Wand : MonoBehaviour {
+
     private GameObject player;
 
-    private void Awake() {
+    private float speed = 1f;
+    private float height = 0.35f;
+
+    private Vector3 originalPosition;
+
+    void Awake() {
         player = GameObject.FindWithTag("Player");
+    }
+
+    void Start() {
+        this.originalPosition = transform.position;
+    }
+ 
+    void Update() {
+        float offset = Mathf.Sin(Time.time * this.speed) * this.height;
+        transform.position = this.originalPosition + new Vector3(0f, offset, 0f);
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         player.GetComponent<PlayerController>().setRemoteTeleportUnlocked(true);
         Destroy(gameObject);
     }
+
 }
