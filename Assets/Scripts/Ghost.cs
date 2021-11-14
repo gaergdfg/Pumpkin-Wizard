f41899b51a7938526ecac7b873cf85f6d356f611
Pe Pe <pszemek.prabucki@gmail.com>
2021-11-14 16:49:35 +0100

    add ghost

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ghost : MonoBehaviour {
+
+    [Header("Unity references")]
+    public Transform pointA;
+    public Transform pointB;
+
+    [Header("Basic info")]
+    private float speed = 1f;
+    private float threshold = 0.05f;
+    private bool goingTowardsB = true;
+
+    void FixedUpdate() {
+        Vector2 targetPosition = this.goingTowardsB ? pointB.position : pointA.position;
+        float diff = Vector2.Distance(targetPosition, transform.position);
+
+        if (diff <= this.threshold) {
+            this.goingTowardsB = !this.goingTowardsB;
+        } else {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, this.speed * Time.fixedDeltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag != "Player") {
+            return;
+        }
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
