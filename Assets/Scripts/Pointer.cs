ba4dc79500dd772c27b9ff913ceabde87dfc14f8
drknzz <kamil.jankowski.x@gmail.com>
2021-11-20 01:30:41 +0100

    fix button pointer
    button pointer now positions itself always in the corner of a button
    
    when changing resolution, only the scale of canvas changes, not the actual dimensions

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -9,6 +9,7 @@ public class Pointer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler 
     private Image pointerImage;
     private AudioManager audioManager;
     private Button button;
+    private GameObject canvas;
 
     private Vector3 pointerPosition;
 
@@ -17,14 +18,15 @@ public class Pointer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler 
         pointerImage = pointer.GetComponent<Image>();
         audioManager = GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>();
         button = GetComponent<Button>();
+        canvas = GameObject.FindWithTag("Canvas");
     }
 
     private void Start() {
         button.onClick.AddListener(DisablePointer);
 
         RectTransform rectTransform = GetComponent<RectTransform>();
-        float width = rectTransform.rect.width;
-        float height = rectTransform.rect.height;
+        float width = rectTransform.rect.width * canvas.transform.localScale.x;
+        float height = rectTransform.rect.height * canvas.transform.localScale.y;
         pointerPosition = new Vector3(transform.position.x + width / 2, transform.position.y + height / 2, transform.position.z);
     }
 
